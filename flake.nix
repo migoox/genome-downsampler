e86d9b3ffe9ca8a5a3f4d6db65f0cdc5f4f72f12
Marek Mytkowski <101755133+mytkom@users.noreply.github.com>
2024-03-30 12:07:51 +0100

    Add nix development flake with cuda (#19)
    * Add nix development flake without cuda
    
    * Add nix cuda dependencies
    
    * Make CUDA compilable under NixOS

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  description = "Nix project environment with clang, cmake and tooling";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
+    in
+    {
+      devShells = forAllSystems
+        (system:
+          let
+            pkgs = import nixpkgs {
+              inherit system;
+              config.allowUnfree = true;
+            };
+          in
+          {
+            default = with pkgs; mkShell.override { stdenv = gcc12Stdenv; }
+              {
+                shellHook = ''
+                  export CUDA_PATH=${pkgs.cudatoolkit}
+                  export LD_LIBRARY_PATH=${pkgs.linuxPackages.nvidia_x11}/lib:${pkgs.ncurses5}/lib
+                  export EXTRA_LDFLAGS="-L/lib -L${pkgs.linuxPackages.nvidia_x11}/lib"
+                  export EXTRA_CCFLAGS="-I/usr/include"
+
+                  zellij
+                '';
+                packages = [
+                  clang-tools
+                  zellij
+                  bear
+                  cmake
+                  cmake-format
+                  zellij
+                ];
+                buildInputs = [
+                  llvmPackages.libcxxClang
+
+                  cudatoolkit linuxPackages.nvidia_x11
+                  ncurses5 binutils
+                ];
+              };
+          }
+        );
+    };
+}
