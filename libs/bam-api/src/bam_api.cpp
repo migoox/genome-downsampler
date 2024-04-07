7cc98e6e348adfeeefe62cd070703d9150c9b518
Michał Okurowski <56317134+migoox@users.noreply.github.com>
2024-04-07 13:40:43 +0200

    Feature 28: Add samtools/htslib library (#29)
    * Added HTSlib find
    
    * Added htslib to ubuntu github actions
    
    * Added htslib submodule
    
    * Added sudo make install
    
    * Added Boost to qmcp-solver
    
    * Added HTSLib to bam_api

--- libs/bam-api/src/bam_api.cpp
+++ libs/bam-api/src/bam_api.cpp
@@ -1,5 +1,7 @@
 #include "../include/bam-api/bam_api.hpp"
 
+#include <htslib/sam.h>
+
 #include <iostream>
 
 void bam_api::BamApi::test_func() { std::cout << "BamApi TEST\n"; }
--- libs/qmcp-solver/src/sequence_network_solver.cpp
+++ libs/qmcp-solver/src/sequence_network_solver.cpp
@@ -1,8 +1,20 @@
 #include "../include/qmcp-solver/sequence_network_solver.hpp"
 
+#include <boost/graph/adjacency_list.hpp>
 #include <iostream>
 
 void qmcp::SequenceNetworkSolver::solve() {
     std::cout << "Not implemented!";
     // TODO(implement the function):
+    // Boost graphs test:
+    typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS>
+        Graph;
+
+    Graph test_graph;
+    boost::add_vertex(test_graph);
+    boost::add_vertex(test_graph);
+    boost::add_vertex(test_graph);
+
+    boost::add_edge(0, 1, test_graph);
+    boost::add_edge(1, 2, test_graph);
 }
