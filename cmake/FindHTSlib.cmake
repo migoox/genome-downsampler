0e17a5507505eb0adc0c2f8f6d9e76f4ffc68228
Michał Okurowski <migoxdesign@gmail.com>
2024-04-07 13:37:26 +0200

    Added HTSLib to bam_api

--- cmake/FindHTSlib.cmake
+++ cmake/FindHTSlib.cmake
@@ -1,30 +1,55 @@
-# From: https://github.com/D-Lo/bamdb/blob/master/cmake/FindHTSlib.cmake
-# - Try to find HTSlib
+# From: https://raw.githubusercontent.com/genome/build-common/master/cmake/FindHTSlib.cmake
+# - Try to find htslib
 # Once done, this will define
 #
-#  HTSlib_FOUND - system has HTSlib
-#  HTSlib_INCLUDE_DIRS - the HTSlib include directories
-#  HTSlib_LIBRARIES - link these to use HTSlib
+#  htslib_FOUND - system has htslib
+#  htslib_INCLUDE_DIRS - the htslib include directories
+#  htslib_LIBRARIES - link these to use htslib
 
+set(HTSLIB_SEARCH_DIRS
+    ${HTSLIB_SEARCH_DIRS}
+    $ENV{HTLSIB_ROOT}
+    /gsc/pkg/bio/htslib
+    /usr
+    /usr/local)
+
+set(_htslib_ver_path "htslib-${htslib_FIND_VERSION}")
 include(LibFindMacros)
 
-# HTSlib dependencies
-libfind_package(HTSLib ZLIB REQUIRED)
+# Dependencies
+libfind_package(HTSlib ZLIB)
 
-# Use pkg-config to get hints about paths
-libfind_pkg_check_modules(HTSlib_PKGCONF HTSlib)
-
-# Locate include dir
+# Include dir
 find_path(
     HTSlib_INCLUDE_DIR
-    NAMES hts.h sam.h
-    PATHS ${HTSlib_PKGCONF_INCLUDE_DIRS}
-    PATH_SUFFIXES include include/htslib)
+    NAMES ${HTSLIB_ADDITIONAL_HEADERS} sam.h
+    PATHS ${HTSLIB_SEARCH_DIRS}
+    PATH_SUFFIXES include include/htslib htslib/${_htslib_ver_path}/htslib
+    HINTS ENV HTSLIB_ROOT)
 
-# Find the library itself
+# Finally the library itself
 find_library(
     HTSlib_LIBRARY
-    NAMES hts
-    PATHS ${HTSlib_PKGCONF_LIBRARY_DIRS})
+    NAMES hts libhts.a hts.a
+    PATHS ${HTSlib_INCLUDE_DIR} ${HTSLIB_SEARCH_DIRS}
+    NO_DEFAULT_PATH
+    PATH_SUFFIXES lib lib64 ${_htslib_ver_path}
+    HINTS ENV HTSLIB_ROOT)
 
+# Set the include dir variables and the libraries and let libfind_process do the rest.
+# NOTE: Singular variables for this library, plural for libraries this lib depends on.
+set(HTSlib_PROCESS_INCLUDES HTSlib_INCLUDE_DIR ZLIB_INCLUDE_DIR)
+set(HTSlib_PROCESS_LIBS HTSlib_LIBRARY ZLIB_LIBRARIES)
 libfind_process(HTSlib)
+
+# Filter the libraries (they should end with .so)
+foreach(lib ${HTSlib_LIBRARIES})
+    if(${lib} MATCHES "\\.so$")
+        list(APPEND filtered_HTSLib_LIBRARIES ${lib})
+    endif()
+endforeach()
+
+set(HTSlib_LIBRARIES ${filtered_HTSLib_LIBRARIES})
+
+message(STATUS "   HTSlib include dirs: ${HTSlib_INCLUDE_DIRS}")
+message(STATUS "   HTSlib libraries: ${HTSlib_LIBRARIES}")
--- libs/bam-api/src/bam_api.cpp
+++ libs/bam-api/src/bam_api.cpp
@@ -1,10 +1,7 @@
 #include "../include/bam-api/bam_api.hpp"
 
-// #include <htslib/sam.h>
+#include <htslib/sam.h>
 
 #include <iostream>
 
-void bam_api::BamApi::test_func() {
-    std::cout << "BamApi TEST\n";
-    // sam_close((htsFile*)nullptr);
-}
+void bam_api::BamApi::test_func() { std::cout << "BamApi TEST\n"; }
