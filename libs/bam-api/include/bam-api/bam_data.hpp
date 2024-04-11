7dc86e59cf5265711b5cd5c167ef59d4185d2696
mytkom <marek.mytkowski.mm@gmail.com>
2024-04-11 15:54:18 +0200

    change quality type

--- libs/bam-api/include/bam-api/bam_data.hpp
+++ libs/bam-api/include/bam-api/bam_data.hpp
@@ -9,19 +9,21 @@ namespace bam_api {
 struct Read {
   int64_t start_ind;
   int64_t end_ind;
-  uint8_t quality;
+  uint32_t quality;
 };
 
 struct AOSPairedReads {
   std::vector<Read> reads;
   std::vector<int64_t> read_pair_map;
+  int64_t ref_genome_length;
 };
 
 struct SOAPairedReads {
   std::vector<int64_t> start_inds;
   std::vector<int64_t> end_inds;
-  std::vector<uint8_t> qualities;
   std::vector<int64_t> read_pair_map;
+  std::vector<uint32_t> qualities;
+  int64_t ref_genome_length;
 };
 
 } // namespace bam_api
