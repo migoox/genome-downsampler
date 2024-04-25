2d4b5f9c5acd41455de4c26cf36c8c00755a1ca9
Michał Okurowski <migoxdesign@gmail.com>
2024-04-25 10:39:00 +0200

    Added the ortools cmake and example

--- libs/qmcp-solver/src/sequence_network_solver.cpp
+++ libs/qmcp-solver/src/sequence_network_solver.cpp
@@ -1,20 +1,40 @@
 #include "../include/qmcp-solver/sequence_network_solver.hpp"
 
-#include <boost/graph/adjacency_list.hpp>
+#include <ortools/graph/max_flow.h>
+
 #include <iostream>
 
 void qmcp::SequenceNetworkSolver::solve() {
     std::cout << "Not implemented!";
     // TODO(implement the function):
-    // Boost graphs test:
-    typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS>
-        Graph;
+    // ortools max flow example basing on the link:
+    // https://developers.google.com/optimization/flow/maxflow#c++
+    // Worth reading:
+    // https://github.com/google/or-tools/blob/stable/ortools/graph/min_cost_flow.h
+    operations_research::SimpleMaxFlow max_flow;
+    std::vector<int64_t> start_nodes = {0, 0, 0, 1, 1, 2, 2, 3, 3};
+    std::vector<int64_t> end_nodes = {1, 2, 3, 2, 4, 3, 4, 2, 4};
+    std::vector<int64_t> capacities = {20, 30, 10, 40, 30,  // NOLINT
+                                       10, 20, 5,  20};     // NOLINT
 
-    Graph test_graph;
-    boost::add_vertex(test_graph);
-    boost::add_vertex(test_graph);
-    boost::add_vertex(test_graph);
-
-    boost::add_edge(0, 1, test_graph);
-    boost::add_edge(1, 2, test_graph);
+    for (int i = 0; i < start_nodes.size(); ++i) {
+        max_flow.AddArcWithCapacity(start_nodes[i], end_nodes[i],  // NOLINT
+                                    capacities[i]);
+    }
+    int status = max_flow.Solve(0, 4);
+    if (status == operations_research::MaxFlow::OPTIMAL) {
+        LOG(INFO) << "Max flow: " << max_flow.OptimalFlow();
+        LOG(INFO) << "";
+        LOG(INFO) << "  Arc    Flow / Capacity";
+        for (std::size_t i = 0; i < max_flow.NumArcs(); ++i) {
+            int i_casted = static_cast<int>(i);
+            LOG(INFO) << max_flow.Tail(i_casted) << " -> "
+                      << max_flow.Head(i_casted) << "  "
+                      << max_flow.Flow(i_casted) << "  / "
+                      << max_flow.Capacity(i_casted);
+        }
+    } else {
+        LOG(INFO) << "Solving the max flow problem failed. Solver status: "
+                  << status;
+    }
 }
