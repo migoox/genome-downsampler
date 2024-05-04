df8363e9ec59055aea3cbe91089866337825ba4c
mytkom <marek.mytkowski.mm@gmail.com>
2024-05-04 21:21:52 +0200

    34: fix red end index bigger than ref genome length bug

--- libs/bam-api/src/bam_api.cpp
+++ libs/bam-api/src/bam_api.cpp
@@ -72,12 +72,14 @@ void bam_api::BamApi::read_bam(const std::filesystem::path& filepath,
     ReadIndex id = 0;
     std::map<std::string, Read> read_map;
     std::string current_qname;
+    hts_pos_t rlen = 0;
     while ((ret_r = sam_read1(infile, in_samhdr, bamdata)) >= 0) {
+        rlen = bam_cigar2rlen(static_cast<int32_t>(bamdata->core.n_cigar),
+                                   bam_get_cigar(bamdata));
         Read current_read{
             .id = id,
             .start_ind = static_cast<ReadIndex>(bamdata->core.pos),
-            .end_ind = static_cast<ReadIndex>(bamdata->core.pos +
-                                              bamdata->core.l_qseq),
+            .end_ind = static_cast<ReadIndex>(bamdata->core.pos + rlen),
             .quality = bamdata->core.qual,
             .is_first_read =
                 static_cast<bool>(bamdata->core.flag & BAM_FREAD1)};
