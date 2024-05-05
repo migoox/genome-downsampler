c2ba3ede978e75b1382fd2b291871b2d6fd9a777
mytkom <marek.mytkowski.mm@gmail.com>
2024-05-05 15:17:00 +0200

    decrement end index by one

--- libs/bam-api/src/bam_api.cpp
+++ libs/bam-api/src/bam_api.cpp
@@ -79,7 +79,7 @@ void bam_api::BamApi::read_bam(const std::filesystem::path& filepath,
         Read current_read{
             .id = id,
             .start_ind = static_cast<ReadIndex>(bamdata->core.pos),
-            .end_ind = static_cast<ReadIndex>(bamdata->core.pos + rlen),
+            .end_ind = static_cast<ReadIndex>(bamdata->core.pos + rlen - 1),
             .quality = bamdata->core.qual,
             .is_first_read =
                 static_cast<bool>(bamdata->core.flag & BAM_FREAD1)};
