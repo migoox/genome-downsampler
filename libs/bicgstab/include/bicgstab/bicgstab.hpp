4f4ba959d45aacbf4e1dca1f90d056b541eac839
Willu12 <boryskurdek@gmail.com>
2024-06-13 01:01:24 +0200

    changed lib to provided by cusparse samples

--- libs/bicgstab/include/bicgstab/bicgstab.hpp
+++ libs/bicgstab/include/bicgstab/bicgstab.hpp
@@ -1,65 +1,105 @@
-#include <ctype.h>
+/*
+ * Copyright 1993-2022 NVIDIA Corporation.  All rights reserved.
+ *
+ * NOTICE TO LICENSEE:
+ *
+ * This source code and/or documentation ("Licensed Deliverables") are
+ * subject to NVIDIA intellectual property rights under U.S. and
+ * international Copyright laws.
+ *
+ * These Licensed Deliverables contained herein is PROPRIETARY and
+ * CONFIDENTIAL to NVIDIA and is being provided under the terms and
+ * conditions of a form of NVIDIA software license agreement by and
+ * between NVIDIA and Licensee ("License Agreement") or electronically
+ * accepted by Licensee.  Notwithstanding any terms or conditions to
+ * the contrary in the License Agreement, reproduction or disclosure
+ * of the Licensed Deliverables to any third party without the express
+ * written consent of NVIDIA is prohibited.
+ *
+ * NOTWITHSTANDING ANY TERMS OR CONDITIONS TO THE CONTRARY IN THE
+ * LICENSE AGREEMENT, NVIDIA MAKES NO REPRESENTATION ABOUT THE
+ * SUITABILITY OF THESE LICENSED DELIVERABLES FOR ANY PURPOSE.  IT IS
+ * PROVIDED "AS IS" WITHOUT EXPRESS OR IMPLIED WARRANTY OF ANY KIND.
+ * NVIDIA DISCLAIMS ALL WARRANTIES WITH REGARD TO THESE LICENSED
+ * DELIVERABLES, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY,
+ * NONINFRINGEMENT, AND FITNESS FOR A PARTICULAR PURPOSE.
+ * NOTWITHSTANDING ANY TERMS OR CONDITIONS TO THE CONTRARY IN THE
+ * LICENSE AGREEMENT, IN NO EVENT SHALL NVIDIA BE LIABLE FOR ANY
+ * SPECIAL, INDIRECT, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, OR ANY
+ * DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
+ * WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS
+ * ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE
+ * OF THESE LICENSED DELIVERABLES.
+ *
+ * U.S. Government End Users.  These Licensed Deliverables are a
+ * "commercial item" as that term is defined at 48 C.F.R. 2.101 (OCT
+ * 1995), consisting of "commercial computer software" and "commercial
+ * computer software documentation" as such terms are used in 48
+ * C.F.R. 12.212 (SEPT 1995) and is provided to the U.S. Government
+ * only as a commercial end item.  Consistent with 48 C.F.R.12.212 and
+ * 48 C.F.R. 227.7202-1 through 227.7202-4 (JUNE 1995), all
+ * U.S. Government End Users acquire the Licensed Deliverables with
+ * only those rights set forth herein.
+ *
+ * Any use of the Licensed Deliverables in individual and commercial
+ * software must include, in the user documentation and internal
+ * comments to the code, the above Disclaimer and U.S. Government End
+ * Users Notice.
+ */
+#include <assert.h>
+#include <cublas_v2.h>
 #include <cuda_runtime.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <cusparse.h>
+#include <stdio.h>   // fopen
+#include <stdlib.h>  // EXIT_FAILURE
+#include <string.h>  // strtok
 
-#include <typeinfo>  // for usage of C++ typeid
+#define CHECK_CUDA(func)                                                         \
+    {                                                                            \
+        cudaError_t status = (func);                                             \
+        if (status != cudaSuccess) {                                             \
+            printf("CUDA API failed at line %d with error: %s (%d)\n", __LINE__, \
+                   cudaGetErrorString(status), status);                          \
+            return EXIT_FAILURE;                                                 \
+        }                                                                        \
+    }
 
-#include "cublas_v2.h"
-#include "cusparse_v2.h"
-#include "helper_cuda.h"
-#include "helper_cusolver.h"
-#include "mmio.h"
-#include "mmio_wrapper.h"
+#define CHECK_CUSPARSE(func)                                                         \
+    {                                                                                \
+        cusparseStatus_t status = (func);                                            \
+        if (status != CUSPARSE_STATUS_SUCCESS) {                                     \
+            printf("cuSPARSE API failed at line %d with error: %s (%d)\n", __LINE__, \
+                   cusparseGetErrorString(status), status);                          \
+            return EXIT_FAILURE;                                                     \
+        }                                                                            \
+    }
 
-// profiling the code
-#define TIME_INDIVIDUAL_LIBRARY_CALLS
+#define CHECK_CUBLAS(func)                                                             \
+    {                                                                                  \
+        cublasStatus_t status = (func);                                                \
+        if (status != CUBLAS_STATUS_SUCCESS) {                                         \
+            printf("CUBLAS API failed at line %d with error: %d\n", __LINE__, status); \
+            return EXIT_FAILURE;                                                       \
+        }                                                                              \
+    }
 
-#define DBICGSTAB_MAX_ULP_ERR 100
-#define DBICGSTAB_EPS 1.E-14f  // 9e-2
+#if defined(NDEBUG)
+#define PRINT_INFO(var)
+#else
+#define PRINT_INFO(var) printf("  " #var ": %f\n", var);
+#endif
 
-#define CLEANUP()                                                             \
-    do {                                                                      \
-        if (x) free(x);                                                       \
-        if (f) free(f);                                                       \
-        if (r) free(r);                                                       \
-        if (rw) free(rw);                                                     \
-        if (p) free(p);                                                       \
-        if (pw) free(pw);                                                     \
-        if (s) free(s);                                                       \
-        if (t) free(t);                                                       \
-        if (v) free(v);                                                       \
-        if (tx) free(tx);                                                     \
-        if (Aval) free(Aval);                                                 \
-        if (AcolsIndex) free(AcolsIndex);                                     \
-        if (ArowsIndex) free(ArowsIndex);                                     \
-        if (Mval) free(Mval);                                                 \
-        if (devPtrX) checkCudaErrors(cudaFree(devPtrX));                      \
-        if (devPtrF) checkCudaErrors(cudaFree(devPtrF));                      \
-        if (devPtrR) checkCudaErrors(cudaFree(devPtrR));                      \
-        if (devPtrRW) checkCudaErrors(cudaFree(devPtrRW));                    \
-        if (devPtrP) checkCudaErrors(cudaFree(devPtrP));                      \
-        if (devPtrS) checkCudaErrors(cudaFree(devPtrS));                      \
-        if (devPtrT) checkCudaErrors(cudaFree(devPtrT));                      \
-        if (devPtrV) checkCudaErrors(cudaFree(devPtrV));                      \
-        if (devPtrAval) checkCudaErrors(cudaFree(devPtrAval));                \
-        if (devPtrAcolsIndex) checkCudaErrors(cudaFree(devPtrAcolsIndex));    \
-        if (devPtrArowsIndex) checkCudaErrors(cudaFree(devPtrArowsIndex));    \
-        if (devPtrMval) checkCudaErrors(cudaFree(devPtrMval));                \
-        if (stream) checkCudaErrors(cudaStreamDestroy(stream));               \
-        if (cublasHandle) checkCudaErrors(cublasDestroy(cublasHandle));       \
-        if (cusparseHandle) checkCudaErrors(cusparseDestroy(cusparseHandle)); \
-        fflush(stdout);                                                       \
-    } while (0)
+typedef struct VecStruct {
+    cusparseDnVecDescr_t vec;
+    double* ptr;
+} Vec;
 
-static void gpu_pbicgstab(
-    cublasHandle_t cublasHandle, cusparseHandle_t cusparseHandle, int m, int n, int nnz,
-    const cusparseMatDescr_t descra, /* the coefficient matrix in CSR format */
-    double* a, int* ia, int* ja,
-    const cusparseMatDescr_t
-        descrm, /* the preconditioner in CSR format, lower & upper triangular factor */
-    double* vm, int* im, int* jm, cusparseSolveAnalysisInfo_t info_l,
-    cusparseSolveAnalysisInfo_t info_u, /* the analysis of the lower and upper triangular parts */
-    double* f, double* r, double* rw, double* p, double* pw, double* s, double* t, double* v,
-    double* x, int maxit, double tol, double ttt_sv);
+//==============================================================================
+
+/// This code allocates. The caller must free.
+void make_test_matrix(int* n_out, int** row_offsets_out, int** columns_out, double** values_out);
+int gpu_BiCGStab(cublasHandle_t cublasHandle, cusparseHandle_t cusparseHandle, int m,
+                 cusparseSpMatDescr_t matA, cusparseSpMatDescr_t matM_lower,
+                 cusparseSpMatDescr_t matM_upper, Vec d_B, Vec d_X, Vec d_R0, Vec d_R, Vec d_P,
+                 Vec d_P_aux, Vec d_S, Vec d_S_aux, Vec d_V, Vec d_T, Vec d_tmp, void* d_bufferMV,
+                 int maxIterations, double tolerance);
