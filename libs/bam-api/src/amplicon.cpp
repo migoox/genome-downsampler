65476d81293943a1a5706ea946c90014d22e621a
mytkom <marek.mytkowski.mm@gmail.com>
2025-08-08 12:24:48 +0200

    fix possible size_t overflow

--- libs/bam-api/src/amplicon.cpp
+++ libs/bam-api/src/amplicon.cpp
@@ -4,5 +4,7 @@ bam_api::Amplicon::Amplicon(Index start, Index end, uint32_t overflow)
     : start(start), end(end), overflow(overflow) {}
 
 bool bam_api::Amplicon::includes(const Read& read) const {
-    return start - overflow <= read.start_ind && read.end_ind < end + overflow;
+    if (start >= overflow)
+        return start - overflow <= read.start_ind && read.end_ind < end + overflow;
+    return start <= read.start_ind && read.end_ind < end + overflow;
 }
